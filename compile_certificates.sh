0626fdce43732dbb52e056b9dd73a4b99787d06d
Deivid Vale <deividrodriguesvale@gmail.com>
2023-01-03 16:20:18 +0100

    First public release.

--- compile_certificates.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-FILES="$HOME/Documents/wanda/certificates/*"
-for f in $FILES
-do
-    g=${f::${#f}-6}
-    dune exec -- onijn $f -o $g.v
-    mv $g.v "$HOME/Documents/wanda/coq_certificates"
-done
