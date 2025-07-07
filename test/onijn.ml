7f6a26795d2d563cdba1deae82cde608439c86a0
Deivid Vale <deividrodriguesvale@gmail.com>
2025-07-07 14:27:42 -0400

    WIP: updates on revision.

--- test/onijn.ml
+++ test/onijn.ml
@@ -70,17 +70,22 @@ Rules: [
   maptlist I (cons Z U) => cons (mapt I Z) (maptlist I U)
 ]
 
-Certificate(Poly) = {
-  J(cons) = Lam[y0;y1].3 ;
-  J(leaf) = Lam[y0].3 ;
-  J(mapt) = Lam[G0;y1].2 + 2*y1 + 3*y1*G0(y1) ;
-  J(maptlist) = Lam[G0;y1].2*y1 + 3*y1*G0(y1) + 3*G0(y1) ;
-  J(nil) = 3 ;
-  J(node) = Lam[y0].3
+Certificate(RREM) = {
+  (
+    [0;1;2;3],
+    [
+      J(cons) = Lam[y0;y1].3 ;
+      J(leaf) = Lam[y0].3 ;
+      J(mapt) = Lam[G0;y1].2 + 2*y1 + 3*y1*G0(y1) ;
+      J(maptlist) = Lam[G0;y1].2*y1 + 3*y1*G0(y1) + 3*G0(y1) ;
+      J(nil) = 3 ;
+      J(node) = Lam[y0].3
+    ]
+  )
 }
 "
 in
 let parsed = parse_from_string p_file wanda_lexer string in
 let processed_file = File.Wanda.process_file parsed in
-
-()
+let t = Rocq.cert_to_rocq processed_file in
+print_endline t
